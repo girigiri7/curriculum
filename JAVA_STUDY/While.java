ceb0894786cd0124cdef0b13baea0efcc6b4f0f0
girigiri7 <milky.9-9vv6_6.peco@outlook.com>
2023-02-23 22:09:42 +0900

    ３章

---
+++ JAVA_STUDY/While.java
@@ -0,0 +1,13 @@
+public class While {
+
+  public static void main(String[] args) { 
+    // 変数の初期化 
+    int i = 1; 
+
+    //1から10までの数字を出力 
+    while (i <= 10){ 
+      System.out.println( i ); 
+      i++; 
+    }
+  } 
+}
