406e70ab430def527b5e0cdfc28d70baf7a92afa
girigiri7 <milky.9-9vv6_6.peco@outlook.com>
2023-03-07 12:30:10 +0900

    課題提出3/7
    演算子

---
+++ JavaScriptjob/1-3/main.js
@@ -0,0 +1,30 @@
+// 問1  a!==10;
+//      b>=10 && b<20;
+
+let a = 15;
+
+if(a !==10){
+console.log(a);
+}
+
+// 2問  x 10～20「成功です」
+//              「失敗です」
+
+let x=10;
+
+if(x>=10 && x<=20){
+  console.log("成功です");
+}else{
+  console.log("失敗です");
+}
+
+// 3問  y 「偶数です」
+//        「奇数です」
+
+let y=0
+
+if(y%2 === 0){
+  console.log("偶数です");
+}else if(y%2 !==0){
+  console.log("奇数です");
+}
