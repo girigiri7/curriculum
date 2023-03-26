3a90ba89c7e8fb8e77a2319d91732448aa1cd636
girigiri7 <milky.9-9vv6_6.peco@outlook.com>
2023-03-26 16:00:54 +0900

    課題再提出3/26
    再提出

--- JavaScriptjob/1-5/main.js
+++ JavaScriptjob/1-5/main.js
@@ -1,7 +1,46 @@
 function createJuice(fruits){
-  fruits="みかん";
   console.log(fruits+"を受け取りました。ジュースにして返します");
   console.log(fruits+"ジュースが届きました");
 }
 
-createJuice();
+createJuice("みかん");
+/*
+//関数の定義
+function japanese(){
+  console.log('おはよう');
+  console.log('こんにちは');
+  console.log('こんばんは');
+  console.log('日本語は時間帯によって挨拶が変わります');
+  console.log('ひらがな');
+  console.log('カタカナ');
+  console.log('漢字');
+  console.log('３種類も文字があります');
+}
+//関数の呼び出し
+japanese()*/
+
+//引数()
+
+//ジュース工場()｛
+//  ジュースを作る処理
+//  注文した所へ発送する処理
+//｝
+
+//関数を動かす=>「関数を呼び出す」
+//果物を受け取る=>引数
+//ジュース工場(果物)<=引数
+//ジュース工場('みかん’);
+
+//戻り値return
+
+//ジュース工場(果物)｛
+//  指定のジュースを作る処理
+//  return ジュース;
+//}
+
+//何をreturnしたいのか「作成したジュース」
+//いつreturnしたいのか「ジュースが作成し終わった時」
+
+//受け取る手順
+//returnで完成したジュースを変数に入れる
+//let orangeJuice=ジュース工場('みかん');
