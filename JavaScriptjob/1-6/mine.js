cca7a5eebc7d6ad4317a60ccb69f5c631eed4baa
girigiri7 <milky.9-9vv6_6.peco@outlook.com>
2023-03-26 19:13:04 +0900

    復習3/26

--- JavaScriptjob/1-6/mine.js
+++ JavaScriptjob/1-6/mine.js
@@ -16,3 +16,46 @@ let car={
 
 console.log('ガソリンは'+car['gass']+'です');
 console.log('ナンバーは'+car['num']+'です');
+/*
+//配列
+let score1=50;
+let score2=60;
+let score3=70;
+let score4=80;
+let score5=90;
+
+let scores=[50,60,70,80,90];
+console.log(scores);
+
+//配列の特定の一部にアクセス
+console.log(scores[2]);
+
+//配列に入っている要素の数 length
+console.log(scores,length);
+
+for(let i=0;i<5;i++){
+  console.log("得点は"+scores[i]+"点です");
+}
+
+for(let i=0;i<scores.length;i++){
+  console.log("得点は"+scores[i]+"点です");
+}
+
+//オブジェクト
+//JavaScriptにおいて「オブジェクト」とは変数や関数をまとめて入れておける箱のようなものだと考えてくださ
+//同じ数値でも意味の違う数値を複数持てる
+//関数も管理できる
+//色々な数値を管理するので0～のインデントではなんの数値かわからなくなる
+//=>インデントに名前をつける
+
+let human={
+  name:'yamada',
+  height:170,
+  weight:60,
+  gender:"男",
+  age:30,
+}
+
+console.log(human.name);
+console.log(human['name']);
+*/
--- JavaScriptjob/1-7/main.js
+++ JavaScriptjob/1-7/main.js
@@ -22,3 +22,74 @@ crem.topping()
 
 let chees=new Taiyaki('チーズ');
 chees.topping()
+/*
+//クラス //設計図(空)
+//インスタンス化 //クラスをコピーして使う使って
+
+let yamada = { 
+    name: 'yamada',
+    height: 170,
+    weight: 60,
+    gender: "男",
+    age: 30,
+}
+//「鈴木」オブジェクト作成
+let suzuki = {
+    name: 'suzuki',
+    height: 180,
+    weight: 70,
+    gender: "男",
+    age: 35,
+}
+
+//クラスの宣言
+class Human{
+  constructor(name,age){//クラスがインスタンス化された時に、一番最初に自動で実行される関数
+    this.name=name;
+    this.age=age;
+  }
+}
+//インスタンス化 Humanクラスを使う
+let yamada=new Human('yamada',25);
+console.log(yamada.name);
+
+//クラスのメリット
+  //書き忘れ防止
+  //コードが見やすい
+  //関数もかけて使える
+class Human{
+  constructor(name,height,weight,gender,age){
+    this.name=name;
+    this.height=height;
+    this.weight=weight;
+    this.gender=gender;
+    this.age=age;
+  }
+}
+
+let yamada=new Human('yamada',170,60,'男',30);
+
+let suzuki=new Human('suzuki',180,70,'男',35);
+
+//クラス内の関数を使用する
+  //クラスの中に関数を書く時functionは不要
+function walk(){
+  console.log('歩きました');
+}
+
+class Human{
+  constructor(name,height,weight,gender,age){
+    this.name=name;
+    this.height=height;
+    this.weight=weight;
+    this.gender=gender;
+    this.age=age;
+  }
+  walk(){
+    console.log('歩きました');
+    console.log(`身長${this.height}cm、体重${this.weight}kg、${this.name}という${this.gender}があるきました`);
+  }
+}
+let yamada=new Human('yamada',170,60,'男',
+30);
+yamada.walk();*/
