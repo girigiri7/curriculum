d5e16408395853a352d5aa44e7cfdf12454fb375
girigiri7 <milky.9-9vv6_6.peco@outlook.com>
2023-04-06 12:47:40 +0900

    課題提出4/6
    要素の取得

---
+++ JavaScriptjob/2-2/js/mine.js
@@ -0,0 +1,20 @@
+// let box=document.querySelector('.box');
+// console.log(box);
+
+// let box=document.querySelector('#box');
+// console.log(box);
+
+// let boxClass=document.querySelectorAll('.box');
+// for(let i=0;i<boxClass.length;i++){
+//   console.log(boxClass[i]);
+// }
+
+// let elems=document.querySelectorAll('form>.box');
+// for(let i=0;i<elems.length;i++){
+//   console.log(elems[i]);
+// }
+
+// let elems=document.querySelectorAll('input[type=radio]');
+// for(let i=0;i<elems.length;i++){
+//   console.log(elems[i]);
+// }
