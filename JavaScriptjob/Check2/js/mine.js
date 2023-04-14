9baa89bf96609da775415da3e399e45093d0fab2
girigiri7 <milky.9-9vv6_6.peco@outlook.com>
2023-04-15 08:01:39 +0900

    2023/4/15

---
+++ JavaScriptjob/Check2/js/mine.js
@@ -0,0 +1,24 @@
+/*let sum=function(a,b,c){
+  return a+b+c;
+};
+
+let total=sum(10,20,30);
+console.log(total);
+
+//アローに置き換え
+let sum=(a,b,c)=>a+b+c;
+let total=sum(20,20,30);
+console.log(total);*/
+
+//-------------------------
+
+/*let double=function(a){
+  return a*2;
+};
+let result=double(10);
+console.log(result);
+
+//アローに置き換え
+let double=a =>a*2;
+let result=double(30);
+console.log(result);*/
